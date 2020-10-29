661f8d0cd2ac180e49a938ca6e130b6cdeb99917
Pasan Anuththara <pasananuththara19@gmail.com>
2020-10-29 19:17:10 +0530

    Update Main.java

--- src/SmartAttendanceSystem/Main.java
+++ src/SmartAttendanceSystem/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Student");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
