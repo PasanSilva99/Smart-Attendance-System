9e53349d1093df81ef5e570a912178b7e4b6688f
Pasan Anuththara <pasananuththara19@gmail.com>
2020-10-29 20:18:46 +0530

    Added Config

--- src/SmartAttendanceSystem/Main.java
+++ src/SmartAttendanceSystem/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
@@ -12,6 +13,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
         primaryStage.setTitle("Student");
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
