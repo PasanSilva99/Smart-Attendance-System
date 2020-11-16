36f44edb0c8f19d9950fb2cc83c45c9f846278fc
Sandaru D Silva <sandarudsilva.dev@gmail.com>
2020-11-16 09:21:02 +0530

    LeaderBoard Modules added! #36
    I`ve added modules to the Leaderboard Scene!

--- src/SmartAttendanceSystem/LeaderBoardMain.java
+++ src/SmartAttendanceSystem/LeaderBoardMain.java
@@ -1,17 +1,74 @@
 package SmartAttendanceSystem;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class LeaderBoardMain implements Initializable {
+
+    @FXML
+    GridPane ModuleView;
+    @FXML
+    ImageView testImage;
+    @FXML
+    Label title_;
+
+    List<ViewItem> LeaderBoardModules;
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        LeaderBoardModules = new ArrayList<ViewItem>();
+        LeaderBoardModules.add(new ViewItem("Bsc in Software Engineering", "19.2 Batch Ply", "Images/modules.png"));
+        LeaderBoardModules.add(new ViewItem("Bsc in Cyber Security", "19.2 Batch Ply", "Images/modules.png"));
+        LeaderBoardModules.add(new ViewItem("Bsc in Computer Networks", "19.2 Batch Ply", "Images/modules.png"));
+        LeaderBoardModules.add(new ViewItem("Bsc in Network Engineering", "20.1 Batch Ply", "Images/modules.png"));
+        LeaderBoardModules.add(new ViewItem("Bsc in Software Engineering", "20.1 Batch Ply", "Images/modules.png"));
+        LeaderBoardModules.add(new ViewItem("MIS", "20.1 Batch Ply", "Images/modules.png"));
+
+
+        //Load the modules
+
+        int r=0, c=0;
+        for (ViewItem module:LeaderBoardModules) {
+            c++;
+            Node crModule = module.CreateModuleItem();
+
+            EventHandler<MouseEvent> ItemClickedEvent = new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent mouseEvent) { ItemClickedF(module);
+                }
+
+            };
+
+            crModule.addEventFilter(MouseEvent.MOUSE_CLICKED, ItemClickedEvent);
+
+
+
+            ModuleView.add(crModule, c, r);
+            ModuleView.setMargin(crModule, new Insets(0,40,20,0));
+            if(c>1) {
+                r++;
+                c=0;
+            }
+
+        }
+
+    }
+    public void ItemClickedF(ViewItem ClickedItem){
+        title_.setText("Clicked On " + ClickedItem.getModuleName());
     }
 }
+
