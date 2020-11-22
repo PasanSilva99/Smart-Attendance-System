6b84cc966340bba35c3b8db057fee9d5b81f5317
Pasan Anuththara <pasananuththara19@gmail.com>
2020-11-22 11:43:50 +0530

    Module Loader SQL Done

--- src/SmartAttendanceSystem/ModulesDAO.java
+++ src/SmartAttendanceSystem/ModulesDAO.java
@@ -1,8 +1,8 @@
 package SmartAttendanceSystem;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import com.sun.glass.ui.View;
+
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,29 +11,60 @@ public class ModulesDAO {
     public static List<ViewItem> ModuleList;
     public static boolean isConnected = false;
 
-    public static List<ViewItem> getModules(){
+    public List<ViewItem> getModules(){
+        if(ModuleList!=null){
+            return ModuleList;
+        }else{
+            fetchModules();
+            return ModuleList;
+        }
+    }
+
+    public static void fetchModules(){
         ModuleList = new ArrayList<ViewItem>();
 
         Connection con = null;
         try{
+            try{
             Class.forName("com.mysql.cj.jdbc.Driver");
 
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sas_db", "root", "root");
 
             System.out.println("Connection to SAS_DB Succeeded.");
-            isConnected = true;
 
-        } catch (ClassNotFoundException | SQLException e) {
+            Statement stmt=con.createStatement();
+            ResultSet rs=stmt.executeQuery("select * from module");
+            while(rs.next()) {
+
+                String ModuleCode = rs.getString(1);
+                String ModuleName = rs.getString(2);
+                String ImagePath = rs.getString(3);
+
+                System.out.println("Fetching : " + ModuleCode + "  " + ModuleName + "  " + ImagePath);
+
+                if (ImagePath == null)
+                    ImagePath = "Images/modules.png";
+
+                ModuleList.add(new ViewItem(ModuleCode, ModuleName, "", ImagePath));
+            }
+            con.close();
+
+            }catch(SQLException e){
+                System.out.println(e);
+                }
+
+    isConnected = true;
+
+        } catch (Exception e) {
             e.printStackTrace();
             System.out.println("SEVER ERROR!!! Connection to SAS_DB Failed!\n"+e);
             isConnected = false;
         }
 
-        return ModuleList;
     }
 
     public static boolean checkModules(){
-        getModules();
+        fetchModules();
         return isConnected;
 
     }
