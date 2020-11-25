32e3b3ad962bd152dd0c8689ced29adbe13f4804
Pasan Anuththara <pasananuththara19@gmail.com>
2020-11-25 11:24:54 +0530

    Create DAO.java

---
+++ src/SmartAttendanceSystem/DAO.java
@@ -0,0 +1,8 @@
+package SmartAttendanceSystem;
+
+public class DAO {
+    public static String SqlDriverClass = "com.mysql.cj.jdbc.Driver";
+    public static String DatabaseUrl = "jdbc:mysql://localhost:3306/sas_db";
+    public static String DBuser = "root";
+    public static String DBpass = "root";
+}
