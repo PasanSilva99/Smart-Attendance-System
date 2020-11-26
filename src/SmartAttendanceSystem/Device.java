81bf4df6ea0d85bf17c9867e7eed6c4e1966aef7
Pasan Anuththara <pasananuththara19@gmail.com>
2020-11-26 08:55:48 +0530

    Added the methods to Device And DeviceDAO
    Added a methodto fetch the IP Records on the mac address and added the functions to REGISTER, UNREGISTER, RECORDLOGIN at DeviceDAO

--- src/SmartAttendanceSystem/Device.java
+++ src/SmartAttendanceSystem/Device.java
@@ -1,5 +1,6 @@
 package SmartAttendanceSystem;
 
+import java.sql.*;
 import java.util.List;
 
 public class Device {
@@ -33,5 +34,47 @@ public class Device {
         DeviceType = deviceType;
     }
 
+    /**
+     * This method updated the IP address list according to the device mac address
+     */
+    public List<String> UpdateIPList() throws SQLException {
+        //SQL Connection
+        Connection con = null;
+        try{
+            // SQL Driver Class
+            Class.forName(DAO.SqlDriverClass);
+            //SQL Connection
+            con = DriverManager.getConnection(DAO.DatabaseUrl, DAO.DBuser, DAO.DBpass);
+
+            //SQL Quarry
+            String sql = "SELECT device_ip_address FROM device_ip WHERE device_mac_address=?";
+
+            //Preparing the statement
+            PreparedStatement statement = con.prepareStatement(sql);
+            // Setting the variables in the statement
+            statement.setString(1, mac_address);
+
+            // Get the reading values
+            ResultSet readData = statement.executeQuery();
+            while (readData.next()){
+
+                String IpAddress = readData.getString(1);
+                if(IpAddress != null) {
+                    IPaddressList.add(IpAddress);
+                    System.out.println("Feting IP Address for " + mac_address + ": " + IpAddress);
+                }
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println("ERROR <!> Fetching IP Addresses for "+mac_address+" !!! "+e);
+        }  finally {
+            assert con != null;
+            con.close();
+        }
+
+
+        return IPaddressList;
+    }
 
 }
