1c716dba3f3641eeab2e2b0dce77c1d43b352c39
Pasan Anuththara <pasananuththara19@gmail.com>
2020-11-30 07:04:01 +0530

    Update to Admin Student page

--- src/AdminPanel/AdminStudentPage.java
+++ src/AdminPanel/AdminStudentPage.java
@@ -1,13 +1,25 @@
 package AdminPanel;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AdminStudentPage implements Initializable {
+    public AnchorPane ap_baseStudent;
+    public GridPane grid_studentView;
+    public Button btn_registerStudent;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    public void btn_registerStudent_Click(ActionEvent actionEvent) {
+        
     }
 }
