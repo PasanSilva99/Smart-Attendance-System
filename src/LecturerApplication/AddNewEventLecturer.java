2663efd01fc171484b4062cd0d4a046e414e6235
Sandaru D Silva <sandarudsilva.dev@gmail.com>
2021-01-18 11:02:06 +0530

    Add event section added to the lecture application

---
+++ src/LecturerApplication/AddNewEventLecturer.java
@@ -0,0 +1,213 @@
+package LecturerApplication;
+
+import LecturerApplication.LecturerEventsPage;
+import Common.BatchDAO;
+import Common.LocationDAO;
+import Common.ModulesDAO;
+import Common.UniEventDAO;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.ResourceBundle;
+
+public class AddNewEventLecturer implements Initializable {
+    public TextField tb_id;
+    public TextField tb_EventName;
+    public ComboBox cmb_eventType;
+    public ComboBox cmb_module;
+    public ComboBox cmb_batch;
+    public ComboBox cmb_startTime;
+    public ComboBox cmb_endTime;
+    public Button btn_save;
+    public TextField tb_lecturer;
+    public Button btn_cancel;
+    public DatePicker dtp_date;
+    public ComboBox cmb_lectureHall;
+
+    public LecturerEventsPage Lecturer;
+
+    String SelectedModuleCode = null;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        updateID();
+        dtp_date.setValue(LocalDate.now());
+        // Populate combo boxes
+        ObservableList<String> EventTypesList = FXCollections.observableArrayList(
+                "Lecture",
+                "Lab",
+                "Other"
+        );
+
+        try {
+            // Populate Event Type ComboBox
+            cmb_eventType.setItems(EventTypesList);
+            cmb_eventType.getSelectionModel().selectFirst();
+        } catch (Exception e) {
+            System.out.println("Fill Event Types error.");
+        }
+
+        try {
+            // Get ad save all the module codes
+            ObservableList<String> ModuleList = ModulesDAO.getModuleCodesOL();
+            // Populate cmb_module
+            cmb_module.setItems(ModuleList);
+            cmb_module.getSelectionModel().selectFirst();
+            SelectedModuleCode = cmb_module.getSelectionModel().getSelectedItem().toString();
+            cmb_module_SelectionCchanged(SelectedModuleCode);
+        } catch (Exception e) {
+            System.out.println("Error in fill Modules" + e.getMessage());
+        }
+
+        try {
+            // Get all Batches
+            ObservableList<String> BatchList = FXCollections.observableArrayList(BatchDAO.getBatchList());
+
+            cmb_batch.setItems(BatchList);
+            cmb_batch.getSelectionModel().selectFirst();
+
+        } catch (Exception e) {
+            System.out.println("Error loading batches" + e.getMessage());
+        }
+        // Generate Time Selections
+        List<String> TimeSelectionsList = new ArrayList<>();
+        for (int h = 0; h < 24; h++) {
+            for (int m = 0; m < 60; m += 30) {
+                String Hours = Integer.toString(h),
+                        Minutes = Integer.toString(m);
+                if (h < 10) Hours = "0" + Hours;
+                if (m < 10) Minutes = "0" + Minutes;
+
+                TimeSelectionsList.add(Hours + ":" + Minutes);
+
+            }
+        }
+
+        try {
+            // List Fir the Time Selections for ComboBox
+            ObservableList<String> TimeSelections = FXCollections.observableArrayList(TimeSelectionsList);
+            // Populate Start Time Combo Box
+            cmb_startTime.setItems(TimeSelections);
+            cmb_startTime.getSelectionModel().select(18);
+            cmb_endTime.setItems(TimeSelections);
+            cmb_endTime.getSelectionModel().select(24);
+
+            cmb_module.getSelectionModel().selectedItemProperty().addListener((options, oldValue, newValue) -> {
+                        System.out.println(newValue);
+                        Platform.runLater(() -> cmb_module_SelectionCchanged(newValue.toString()));
+                    }
+            );
+        } catch (Exception e) {
+            System.out.println("Error fetching Lecturer");
+        }
+
+        // Populate locations
+        try{
+            ObservableList<String> locations = FXCollections.observableArrayList(new LocationDAO().getLocationCodeList());
+            cmb_lectureHall.setItems(locations);
+            cmb_lectureHall.getSelectionModel().selectFirst();
+
+        }catch (Exception e){
+            System.out.println("Error loading location or No locations registered" + e.getMessage());
+        }
+
+
+    }
+
+    /**
+     * This event will handle the Module Selection changed Event
+     *
+     * @param selectedValue
+     */
+    public void cmb_module_SelectionCchanged(String selectedValue) {
+        // Save the value
+        SelectedModuleCode = selectedValue;
+        // get lecturer name according to selected module
+        String LecturerName = ModulesDAO.getLecturerName(SelectedModuleCode);
+        tb_lecturer.setText(LecturerName);
+    }
+
+    /**
+     * This will get a unique key for the event
+     */
+    public void updateID() {
+        String newID = generateNewID();
+        boolean isDuplicate = new UniEventDAO().CheckDuplicateID(newID);
+        if (!isDuplicate) {
+            tb_id.setText(newID);
+        } else {
+            updateID();
+        }
+    }
+
+    /**
+     * This returns a Random Alphanumeric String
+     *
+     * @return Random Alphanumeric String
+     */
+    public String generateNewID() {
+        int leftLimit = 48; // numeral '0'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 5;
+        Random random = new Random();
+
+        String generatedString = random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+
+        System.out.println(generatedString);
+        return generatedString;
+    }
+
+    public void setMainPage(LecturerEventsPage lecturer){
+        Lecturer = lecturer;
+    }
+
+    public void btn_saveClick(ActionEvent actionEvent) {
+        String event_id = tb_id.getText();
+        String event_name = tb_EventName.getText();
+        String module_code = cmb_module.getSelectionModel().getSelectedItem().toString();
+        String start_time = dtp_date.getValue() + " " + cmb_startTime.getSelectionModel().getSelectedItem().toString();
+        String end_time = dtp_date.getValue() + " " + cmb_endTime.getSelectionModel().getSelectedItem().toString();
+        String lecturer = tb_lecturer.getText();
+        String batch = cmb_batch.getSelectionModel().getSelectedItem().toString();
+        String event_type = cmb_eventType.getSelectionModel().getSelectedItem().toString();
+        String location = cmb_lectureHall.getSelectionModel().getSelectedItem().toString();
+
+
+        new UniEventDAO().AddEvent(event_id, event_name, module_code, start_time, end_time, lecturer, batch, event_type, location);
+
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setContentText("Event "+event_id+ " Saved Successfully");
+        alert.setTitle("✔ Success");
+        alert.show();
+
+        Lecturer.RefreshData();
+
+        Stage stage = (Stage) tb_id.getScene().getWindow();
+        stage.close();
+    }
+
+    public void btn_cancel_click(ActionEvent actionEvent) {
+        Stage stage = (Stage) tb_id.getScene().getWindow();
+        stage.close();
+    }
+
+
+    public void dateChanged(ActionEvent actionEvent) {
+        System.out.println(dtp_date.getValue());
+    }
+}
--- src/LecturerApplication/LecturerEventsPage.java
+++ src/LecturerApplication/LecturerEventsPage.java
@@ -1,14 +1,108 @@
 package LecturerApplication;
 
+import AdminPanel.AddNewEvent;
+import Common.EventDetailView;
+import Common.UniEvent;
+import Common.UniEventDAO;
+import Common.ViewItemWithBadge;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-
+import javafx.geometry.Insets;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
 
 import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class LecturerEventsPage implements Initializable {
+    public GridPane grid_eventViewLecturer;
+
+    public AnchorPane baseLecturer;
+
+    List<UniEvent> EventList = new ArrayList<>();
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        RefreshData();
 
+    }
+
+    public void setMainPage(AnchorPane mainPage){baseLecturer = mainPage; }
+
+    public void RefreshData(){
+        EventList = new UniEventDAO().getLectureList();
+
+        int c=0, r=0;
+        for (UniEvent event:EventList) {
+            c++;
+            ViewItemWithBadge item = new ViewItemWithBadge( ""+event.getModuleO(),""+event.getEventName(),""+event.getStartTime().substring(10)+" - "+ event.getEndTime().substring(10), event.getBatch(), event.getLocation());
+
+            AnchorPane lectureItem = item.getControl();
+            grid_eventViewLecturer.add(lectureItem, c, r);
+
+            EventHandler<MouseEvent> onMouseClick = new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent mouseEvent) {
+                    Platform.runLater(()-> itemClick(event));
+                }
+            };
+
+            lectureItem.addEventFilter(MouseEvent.MOUSE_CLICKED, onMouseClick);
+
+            GridPane.setMargin(lectureItem, new Insets(10,10,10,10));
+            if(c>1){
+                c=0;
+                r++;
+            }
+
+        }
+    }
+
+    private void itemClick(UniEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("EventDetailViewLecturer.fxml"));
+            AnchorPane page = loader.load();
+            EventDetailView controler = loader.getController();
+            controler.setEvent(event);
+            // This Is the main Anchor pane.
+            // To go back we need to send it to the page that we load
+            controler.setBaseAdmin(baseLecturer);
+            baseLecturer.getChildren().clear();
+            baseLecturer.getChildren().setAll(page);
+
+        }catch (Exception e){
+            System.out.println("Couldn't Load the Page");
+        }
+
+    }
+
+    public void btn_addNewEvent_click(ActionEvent actionEvent) {
+        try {
+            FXMLLoader loder = new FXMLLoader();
+            loder.setLocation(getClass().getResource("AddNewEventLecturer.fxml"));
+            Parent root = loder.load();
+            AddNewEventLecturer controller = loder.getController();
+            controller.setMainPage(this);
+            Stage primaryStage = new Stage();
+            primaryStage.setTitle("Add New Event");
+            primaryStage.setScene(new Scene(root, 380, 550));
+            primaryStage.show();
+
+            Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
+            primaryStage.setX((primScreenBounds.getWidth() - primaryStage.getWidth()) / 2);
+            primaryStage.setY((primScreenBounds.getHeight() - primaryStage.getHeight()) / 2);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
